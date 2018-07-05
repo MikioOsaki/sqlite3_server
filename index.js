98cc939458417937406afd4fe10c64d35ee567c9
mikioosaki <mikio.osaki@posteo.de>
2018-07-05 17:49:22 +0200

    Change content type to json, fix favicon not found

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ var app = express();
 
 var port = process.env.PORT || 8081;
 
+var favicon = require('serve-favicon');
+var path = require('path');
+
+
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 //retrive data
 app.post('/upload', function (req, res) { //post
     res.writeHead(200, {
-        'Content-Type': 'text/html'
+        'Content-Type': 'application/json'
     });
+
     res.write("<h1>Landing Page for SQLite3 Data Server</h1>");
     res.write("<p>request: " + req.body + "</p>");
+    res.write("<p>Yey! :)</p>");
     res.end();
 });
 
