5b9f41f41d8e895058d9cad9b92928c185b0397f
mikioosaki <mikio.osaki@posteo.de>
2018-07-05 11:26:26 +0200

    Build server with express
    instad of http module

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+var port = process.env.PORT || 8081;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
     res.write("<h1>Landing Page for SQLite3 Data Server</h1>");
     res.end();
-}).listen(process.env.PORT ||8080);
+});
+
+app.listen(port, function () {
+    console.log('running app on PORT: ' + port);
+});
+
+//________________________________sqlite3_______________________________
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
+
+db.serialize(function () {
+
+    db.run('CREATE TABLE lorem (info TEXT)');
+    var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+
+    for (var i = 0; i < 10; i++) {
+        stmt.run('Ipsum ' + i);
+    }
+
+    stmt.finalize();
+
+    db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
+        console.log(row.id + ': ' + row.info);
+    });
+});
+
+db.close();
